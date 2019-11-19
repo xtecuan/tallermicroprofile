f4ae4b048e1415c8eeb52f0ce90540219575c033
Tadeo Rivera <xtecuan@gmail.com>
2019-11-19 10:33:14 -0600

    Taller Microprofile DevFest 2019

---
+++ src/main/java/sv/org/devfest/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package sv.org.devfest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+
+}
